9ab0c5260ad88101fe48f97f91e1fcc3b3f2efe3
Nguyen, Anh <anh.nguyen@slu.edu>
2019-04-07 14:31:04 -0500

    Update BlackjackView.java

--- BlackjackView.java
+++ BlackjackView.java
@@ -43,16 +43,4 @@ public class BlackjackView
       this.gameplayGUI.setVisible(false);
       frame.add(this.endMessage);      
    }
-   
-   public static void main(String[] args)
-   {
-       TimeUnit time = TimeUnit.SECONDS;
-       BlackjackView game = new BlackjackView();   
-       game.displayMenu();
-       time.sleep(3);
-       game.displayGame();
-       time.sleep(3);
-       game.displayEndMessage();
-   }
-
 }
