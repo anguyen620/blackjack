496d33a3f5af2a5243ef52898826300c61c79320
Sarpong, David K <david.sarpong@slu.edu>
2019-04-07 14:18:34 -0500

    add a tester main method

--- BlackjackView.java
+++ BlackjackView.java
@@ -1,13 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.concurrent.*;
 
 public class BlackjackView 
 {
-   JFrame frame;
-   EndMessage endMessage;
-   Opening opening;
-   GamePlayGUI gameGUI;
+   protected JFrame frame;
+   protected EndMessage endMessage;
+   protected Opening opening;
+   protected GamePlayGUI gameGUI;
 
    BlackjackView()
    {
@@ -19,6 +20,7 @@ public class BlackjackView
       frame.pack();
       frame.setVisible(true);
    }
+   
    public void displayMenu()
    {
       if (this.endMessage != null)
@@ -27,16 +29,30 @@ public class BlackjackView
       }
       this.opening.setVisible(true);
    }
+   
    public void displayGame()
    {
       this.gameGUI = new GameplayGUI();
       this.opening.setVisible(false);
       this.frame.add(this.gameGUI);
    }
+   
    public void displayEndMessage(String winner, int dealerScore, int playerScore, int computerScore)
    {
       this.endMessage = new EndMessage(winner, dealerScore, playerScore, computerScore);
       this.gameplayGUI.setVisible(false);
       frame.add(this.endMessage);      
+   }
+   
+   public static void main(String[] args)
+   {
+       TimeUnit time = TimeUnit.SECONDS;
+       BlackjackView game = new BlackjackView();   
+       game.displayMenu();
+       time.sleep(10);
+       game.displayGame();
+       time.sleep(10);
+       game.displayEndMessage();
+   }
    }
 }
