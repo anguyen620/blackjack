0cafa9581ea83130da07841ca979f0a9286f8898
Sarpong, David K <david.sarpong@slu.edu>
2019-04-07 14:20:54 -0500

    Update BlackjackView.java

--- BlackjackView.java
+++ BlackjackView.java
@@ -49,10 +49,10 @@ public class BlackjackView
        TimeUnit time = TimeUnit.SECONDS;
        BlackjackView game = new BlackjackView();   
        game.displayMenu();
-       time.sleep(10);
+       time.sleep(3);
        game.displayGame();
-       time.sleep(10);
+       time.sleep(3);
        game.displayEndMessage();
    }
-   }
+
 }
