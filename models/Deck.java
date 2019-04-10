d8df557f555e6808858adb397acce290ee4a58ef
Sarpong, David K <david.sarpong@slu.edu>
2019-04-10 12:47:08 -0500

    Upload New File

---
+++ models/Deck.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Deck
+{
+   protected ArrayList<Card> cards;
+   protected Random rand;
+
+   public Deck()
+   {
+      cards = new ArrayList<>();
+      rand = new Random();
+   }
+
+   public Deck(ArrayList<Card> cards)
+   {
+      this.cards = cards;
+      this.rand = new Random();
+   }
+
+   public void deal(AbstractPlayer player)
+   {
+      int deckSize = getSize();
+      int index = rand.nextInt(deckSize);
+      Card card = getCard(index);
+
+      player.addToHand(card);
+   }
+
+   public int getSize()
+   {
+      return cards.size();
+   }
+
+   public Card getCard(int index)
+   {
+      return cards.get(index);
+   }
+}
