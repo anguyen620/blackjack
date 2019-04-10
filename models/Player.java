5bff115ebf4500716b9ba26f1e003a24af8d911a
Sarpong, David K <david.sarpong@slu.edu>
2019-04-10 12:47:21 -0500

    Upload New File

---
+++ models/Player.java
@@ -0,0 +1,47 @@
+public class Player extends AbstractPlayer
+{
+   protected Hand hand;
+   protected int funds;
+   protected int bet;
+
+   public Player()
+   {
+      super();
+      hand = new Hand();
+      funds = 0;
+      bet = 0;
+   }
+
+   public Player(Hand hand, int funds)
+   {
+      super();
+      this.hand = hand;
+      this.funds = funds;
+      this.bet = bet;
+   }
+
+   public void addToHand(Card card)
+   {
+      hand.add(card);
+   }
+
+   public ArrayList<Card> getHand()
+   {
+      hand.getHand();
+   }
+
+   public void changeBet(int newBet)
+   {
+      bet = newBet
+   }
+
+   public int getBet()
+   {
+      return bet;
+   }
+
+   public void setFunds(int amount)
+   {
+      funds = amount;
+   }
+}
