a47441c9aa61b5690f60e5ce7593b7472e18dd0c
Sarpong, David K <david.sarpong@slu.edu>
2019-04-12 12:43:03 -0500

    Upload New File

---
+++ models/unit_tests/DealerTest.java
@@ -0,0 +1,23 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class DealerTest
+{
+   @Test
+   public void constructorTest1()
+   {
+   
+   }
+
+   @Test
+   public void constructorTest2()
+   {
+
+   }
+
+   @Test
+   public void dealTest()
+   {
+    
+   }
+}
