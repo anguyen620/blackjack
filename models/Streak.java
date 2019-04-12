3a8753b6edde44d8a220ec7524c4564e3bfe2257
Nguyen, Anh <anh.nguyen@slu.edu>
2019-04-12 12:32:38 -0500

    Update Streak.java

--- models/Streak.java
+++ models/Streak.java
@@ -3,8 +3,20 @@ import java.io.Serializable;
 public class Streak implements Serializable
 {
    int streak;
-   Streak()
+   Streak(int currentStreak)
+   {
+      this.streak = currentStreak;
+   }
+   public void resetStreak()
    {
       this.streak = 0;
+   }
+   public int getStreak()
+   {
+      return this.streak;
+   }
+   public void increment()
+   {
+      this.streak = this.streak + 1;
    }
 }
