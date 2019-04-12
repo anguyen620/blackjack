cfd2aed45b9ea0d4f5746504a83cade88c9b0cc5
Nguyen, Anh <anh.nguyen@slu.edu>
2019-04-12 12:44:34 -0500

    Update Streak.java

--- models/Streak.java
+++ models/Streak.java
@@ -3,12 +3,22 @@ import java.io.Serializable;
 public class Streak implements Serializable
 {
    int streak;
+   String fileName;
    Streak(String filename) throws Exception
    {
+      this.fileName = filename;
       FileInputStream file = newFileInputStream(filename);
       ObjectInputStream in = new ObjectInputStream(file);
       in.readObject(this.streak);
       in.close();
+      file.close();
+   }
+   public void saveToFile()
+   {
+      FileOutputStream file = new FileOutputStream(fileName);
+      ObjectOutputStream out = new ObjectOutputStream(file);
+      out.write(this.streak);
+      out.close();
       file.close();
    }
    public void resetStreak()
