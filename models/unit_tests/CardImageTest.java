38730f723a5e466ec0e53cb6c5bb6e162cc2b5fc
Sarpong, David K <david.sarpong@slu.edu>
2019-04-12 12:48:44 -0500

    Upload New File

---
+++ models/unit_tests/CardImageTest.java
@@ -0,0 +1,45 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import javax.swing.JLabel;
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CardImage
+{
+   private static final String PATH = "../../images/image1.jpeg";
+
+   @Test
+   public void constructorTest()
+   {
+        
+   }
+
+   @Test
+   public void generateImageTest() throws IOException
+   {
+      File file = new File(PATH);
+      BufferedImage pic = ImageIO.read(file);
+      JLabel image = new JLabel(new ImageIcon(pic));
+
+      CardImage cardImage = new CardImage(path);
+
+      assertEquals(image, cardImage.getCardImage());
+   }
+
+   @Test
+   public void getCardImageTest()
+   {
+      File file = new File(PATH);
+      BufferedImage pic = ImageIO.read(file);
+      JLabel image = new JLabel(new ImageIcon(pic));
+
+      CardImage cardImage = new CardImage(path);
+
+      assertEquals(image, cardImage.getCardImage());
+   }
+}
