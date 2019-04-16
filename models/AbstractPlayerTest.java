50e11e71fa6602f791da6d29dedc522d541c0b05
Nguyen, Anh <anh.nguyen@slu.edu>
2019-04-16 16:00:08 -0500

    Upload New File

---
+++ models/AbstractPlayerTest.java
@@ -0,0 +1,19 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+class AbstractPlayerTest.java
+{
+   @Test public void testConstructor()
+   {
+      AbstractPlayer player = new AbstractPlayer();
+      assertTrue(player.getScore() == 0);
+   }
+   @Test public void testUpdateScore()
+   {  
+      Hand hand = new Hand();
+      hand.add(SPADES, 10, new CardImage("./images/image2");
+      AbstractPlayer player = new AbstractPlayer(hand);
+      player.updateScore();
+      assertTrue(player.getScore() == 10);
+   }
+}
