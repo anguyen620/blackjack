dcfc86be2a2a9ad30563645975403750398bfe2b
Sarpong, David K <david.sarpong@slu.edu>
2019-04-16 16:36:41 -0500

    Update Dealer.java

--- models/Dealer.java
+++ models/Dealer.java
@@ -21,11 +21,14 @@ public class Dealer extends AbstractPlayer
    }
    
    @Override
-   public void hit()
+   public void hit(Card card)
    {
       if (getScore() < 17)
       {
-
+         addToHand(card);
       }
+      
+    //   System.out.println("Dealer should not be hitting right now"); // debug statement
+    //   return;
    }
 }
