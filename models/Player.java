0aa4ea928cbd83e319cb86e9aabccfb58e5c12d9
Nguyen, Anh <anh.nguyen@slu.edu>
2019-04-16 18:35:04 -0500

    Update Player.java

--- models/Player.java
+++ models/Player.java
@@ -1,35 +1,14 @@
 public class Player extends AbstractPlayer
 {
-   protected int funds;
-   protected int bet;
 
    public Player()
    {
       super();
-      funds = 0;
-      bet = 0;
    }
 
    public Player(Hand hand, int funds)
    {
       super(hand);
-      this.funds = funds;
-      this.bet = bet;
-   }
-
-   public void changeBet(int newBet)
-   {
-      bet = newBet;
-   }
-
-   public int getBet()
-   {
-      return bet;
-   }
-
-   public void setFunds(int amount)
-   {
-      funds = amount;
    }
    
    public void split()
