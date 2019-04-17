db479005d2cf854e4aebde06ccb61bf84e1d31ee
Sarpong, David K <david.sarpong@slu.edu>
2019-04-16 19:38:29 -0500

    Update AbstractPlayerTest.java

--- models/AbstractPlayerTest.java
+++ models/AbstractPlayerTest.java
@@ -3,15 +3,18 @@ import static org.junit.Assert.*;
 
 class AbstractPlayerTest
 {
-   @Test public void testConstructor()
+   @Test 
+   public void testConstructor()
    {
       AbstractPlayer player = new AbstractPlayer();
       assertTrue(player.getScore() == 0);
    }
-   @Test public void testUpdateScore()
+   
+   @Test 
+   public void testUpdateScore()
    {  
       Hand hand = new Hand();
-      hand.add(new Card(SPADES, 10, new CardImage("./images/image2")));
+      hand.add(new Card(Card.SPADES, 10, new CardImage("./images/image2")));
       AbstractPlayer player = new AbstractPlayer(hand);
       player.updateScore();
       assertTrue(player.getScore() == 10);
