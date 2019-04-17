d000ebc91b1ab6962fcae9a2205f5a7a40841070
Nguyen, Anh <anh.nguyen@slu.edu>
2019-04-17 09:49:57 -0500

    Upload New File

---
+++ StreakTest.java
@@ -0,0 +1,58 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.io.*;
+
+class StreakTest
+{
+   @Test
+   public void testConstructor()
+   {
+      Streak streak = new Streak("streak.dat");
+      assertTrue(streak.getStreak() == 0);
+   } 
+
+//Depends on increment, constructor working
+   @Test
+   public void testSaveToFile()
+   {
+      Streak streak = new Streak("streak.dat");
+      streak.increment();
+      streak.saveToFile();
+      try
+      {
+         FileInputStream file = new FileInputStream("streak.dat");
+         ObjectInputStream in = new ObjectInputStream(file);
+         int temp = (Integer)in.readObject();
+         in.close();
+         file.close();
+      }
+      catch (Exception error)
+      {
+         System.out.println("Cannot retrieve streak data");
+      }
+      assertTrue(temp == 1);
+   }
+
+//Depends upon constructor working
+   @Test
+   public void testResetStreak()
+   {
+      Streak streak = new Streak("streak.dat");
+      streak.increment();
+      streak.resetStreak();
+      assertTrue(streak.getStreak() == 0);
+   }
+   @Test
+   public void testGetStreak()
+   {
+      Streak streak = new Streak("streak.dat");
+      assertTrue(streak.getStreak() == 0);
+   }
+   @Test
+   public void testIncrement()
+   {
+      Streak streak = new Streak("streak.dat");
+      streak.increment();
+      assertTrue(streak.getStreak() == 1);
+   }
+}
