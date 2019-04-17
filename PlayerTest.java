452ede38d9ce866ce3bd239b9db5260f45e9eb42
Sarpong, David K <david.sarpong@slu.edu>
2019-04-17 11:17:25 -0500

    Update PlayerTest.java

--- PlayerTest.java
+++ PlayerTest.java
@@ -11,16 +11,10 @@ class PlayerTest
    }
    
    @Test 
-   public void testSplit()
-   {
-      
-   }
-   
-   @Test 
    public void testHit()
    {
       Player player = new Player();
-      player.hit(new Card(Card.SPADES, 10, new CardImage("./images/ace_of_hearts.png")));
+      player.hit(new Card(Suit.SPADES, 10, new CardImage("./images/ace_of_hearts.png")));
       assertTrue(player.getScore() == 10);
    }
 }
