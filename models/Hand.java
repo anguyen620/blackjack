7213b71f9e737788dad4f9e20eee00d4c31259d7
Sarpong, David K <david.sarpong@slu.edu>
2019-04-16 19:01:58 -0500

    Update Hand.java

--- models/Hand.java
+++ models/Hand.java
@@ -7,18 +7,11 @@ public class Hand
    public Hand()
    {
       cards = new ArrayList<>();
-      handTotal = 0;
-   }
-
-   public int getHandTotal()
-   {
-      return handTotal;
    }
 
    public void add(Card card)
    {
       cards.add(card);
-      handTotal++;
    }
 
    public ArrayList<Card> getHand()
