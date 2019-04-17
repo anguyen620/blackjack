565c482b37be2680e112dcd968adb7eca288c2c4
Sarpong, David K <david.sarpong@slu.edu>
2019-04-17 11:10:44 -0500

    Update HandTest.java

--- HandTest.java
+++ HandTest.java
@@ -9,31 +9,25 @@ public class HandTest
    public void constructorTest()
    {
       Hand hand = new Hand();
-      assertEquals(0, hand.getHandTotal());
+      assertEquals(hand, new Hand());
    }
 
    @Test
    public void addTest()
    {
-      Card card = new Card("Hearts", 9, "../../images/image1.jpeg");
+      Card card = new Card(Suit.HEARTS, 9, "../../images/image1.jpeg");
       Hand hand = new Hand();
       hand.add(card);
 
-      assertEquals(1, hand.getHandTotal());
+      assertEquals(1, hand.getHand().size());
    }
 
-   @Test
-   public void getHandTotalTest()
-   {
-      Hand hand = new Hand();
-      assertEquals(0, hand.getHandTotal());
-   }
 
    @Test
    public void getHandTest()
    {
       Hand hand = new Hand();
-      Card card = new Card("Hearts", 9, "../../images/image1.jpeg");
+      Card card = new Card(Suit.HEARTS, 9, "../../images/image1.jpeg");
       hand.add(card);
 
       ArrayList<Card> cardsHand = new ArrayList<>();
