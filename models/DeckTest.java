b20fa2e421404809d4669b83d0d75830d4c40f82
Nguyen, Anh <anh.nguyen@slu.edu>
2019-04-16 20:21:29 -0500

    Upload New File

---
+++ models/DeckTest.java
@@ -0,0 +1,41 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+class DeckTest
+{
+   @Test public void testConstructor()
+   {
+      Deck deck = new Deck();
+      assertTrue(deck.getSize() == 0);
+   }
+   @Test public void testDeal()
+   {
+      Deck deck = new Deck();
+      Player player = new Player();
+      deck.deal(player);
+      assertFalse(player.getScore() == 0);
+   }
+   @Test public void testGetSize()
+   {
+      Deck deck = new Deck();
+      assertTrue(deck.getSize() == 0);     
+   }
+   @Test public void testGetCard()
+   {
+      ArrayList<Card> cards = new ArrayList<Card>();
+      Card card = new Card(SPADE, 10, "./images/image2")
+      cards.add(card);
+      Deck deck = new Deck(cards);
+      assertTrue(card == deck.getCard(0));
+   }
+   @Test public void testRemoveCard()
+   {
+      ArrayList<Card> cards = new ArrayList<Card>();
+      Card card = new Card(SPADE, 10, "./images/image2")
+      cards.add(card);
+      Deck deck = new Deck(cards);
+      if (deck.getSize()) == 1)
+         deck.removeCard(card);
+      assertTrue(deck.getSize() == 0);
+   }
+}
