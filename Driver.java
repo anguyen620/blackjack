d834a87f3f65be5af17645575c3065e1f39ab4d4
Sarpong, David K <david.sarpong@slu.edu>
2019-04-30 18:28:21 -0500

    Replace Driver.java

--- Driver.java
+++ Driver.java
@@ -1,15 +1,9 @@
-import java.util.concurrent.TimeUnit;
-
 class Driver
 {
-    public static void main(String[] args) throws InterruptedException
+   public static void main(String[] args) throws InterruptedException
    {
-       TimeUnit time = TimeUnit.SECONDS;
-       BlackjackView game = new BlackjackView();   
-       time.sleep(3);
-       game.displayGame();
-       time.sleep(3);
-       game.displayEndMessage("You win!", 11, 18, 0);
+      BlackjackView view = new BlackjackView();   
+      BlackjackController controller = new BlackjackController(view);
    }
 }
 
