d8c9cf574e1c1821b68d0178596c7815a8cc8297
Nguyen, Anh <anh.nguyen@slu.edu>
2019-05-02 16:48:40 -0500

    Replace Driver.java

--- Driver.java
+++ Driver.java
@@ -1,9 +1,17 @@
+import java.awt.event.*;
+
 class Driver
 {
-   public static void main(String[] args) throws InterruptedException
+   public static void main(String[] args)
    {
-      BlackjackView view = new BlackjackView();   
-      BlackjackController controller = new BlackjackController(view);
+      BlackjackView view = new BlackjackView();
+      BlackjackController controller = new BlackjackController(view);   
+      view.displayMenu();
+      view.addSinglePlayerModeListener(controller.singleModeListener);
+      view.addVersusModeListener(controller.versusModeListener);
+      view.addHelpListener(controller.helpButtonListener);
+      view.addHitListener(controller.hitListener);
+      view.addStandListener(controller.standListener);
    }
 }
 
