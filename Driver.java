cb8fcbae49e43bf9eb80c00eb1fa8e76a75157fc
Nguyen, Anh <anh.nguyen@slu.edu>
2019-05-02 21:33:28 -0500

    Replace Driver.java

--- Driver.java
+++ Driver.java
@@ -5,13 +5,9 @@ class Driver
    public static void main(String[] args)
    {
       BlackjackView view = new BlackjackView();
-      BlackjackController controller = new BlackjackController(view);   
-//      view.displayMenu();
-//      view.addSinglePlayerModeListener(controller.singleModeListener);
-//      view.addVersusModeListener(controller.versusModeListener);
-//      view.addHelpListener(controller.helpButtonListener);
-//      view.addHitListener(controller.hitListener);
-//      view.addStandListener(controller.standListener);
+      BlackjackModel model = new BlackjackModel();
+      BlackjackController controller = new BlackjackController(view, model);   
+      view.displayMenu();
    }
 }
 
